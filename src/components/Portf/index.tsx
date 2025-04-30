9ee60ede461a3c975094aee778c0f47c91818fae
DevJofre <jofretomas9@gmail.com>
2025-04-29 22:45:08 -0300

    x

--- src/components/Portf/index.tsx
+++ src/components/Portf/index.tsx
@@ -39,7 +39,7 @@ export function Portfolio() {
     return (
       <Box textAlign="center" py={10}>
         <Heading as="h2" size="xl" mb={2}>
-          Teste
+          Portfólio
         </Heading>
         <Text mb={6}>Conheça alguns sites profissionais que já criamos</Text>
         <Flex
