2815f8d0d350ffcbc5dea1106a8220c82a9f69c6
Jofre Tomas <jofre@acheipneus.com.br>
2025-05-23 17:19:10 -0300

    feat: atualização do link do whats

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -90,7 +90,7 @@ export function Navbar() {
             label="GitHub"
           />
           <SocialIcon
-            href="https://w.app/shcgkt"
+            href="https://wa.me/5547992728639?text=Oi%20sou%20o%20Jofre%2C%20como%20posso%20ajudar%3F"
             icon={<FaWhatsapp fontSize="1.5rem" />} 
             label="WhatsApp"
           />
